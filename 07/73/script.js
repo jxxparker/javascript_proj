b0497366b82e591f5add2c7a4f6e126b7ef30524
jihun <jxxparker@gmail.com>
2023-03-20 21:00:25 -0500

    pushing 73 changes

--- 07/73/script.js
+++ 07/73/script.js
@@ -1,19 +1,11 @@
 'use strict';
 
-const x = function() {
-    console.log(23);
-}
-
-
-document.querySelector('.check').addEventListener
-('click', function() {
+document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
-    
+
     if (!guess) {
-        document.querySelector('.message').textContent = '👻 No Number';
+        document.querySelector('.message').textContent = 'No Number'
     }
 });
-
-
 
