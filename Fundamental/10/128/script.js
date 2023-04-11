eb7f1cf830a84b743d65e1de733e1906933be652
jihun <jxxparker@gmail.com>
2023-04-10 21:00:02 -0500

    pushing new section

--- Fundamental/10/128/script.js
+++ Fundamental/10/128/script.js
@@ -1,25 +1,21 @@
-'use strict';
+"use strict";
 
 const bookings = [];
 
-const createBooking = function(
-  flightNum, 
-  numPassengers = 1, 
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
   price = 199 * numPassengers
-  ) 
-{
-  // numPassengers = numPassengers || 1;
-  // price = price || 199;
-  
+) {
   const booking = {
     flightNum,
     numPassengers,
     price,
-  }
+  };
   console.log(booking);
   bookings.push(booking);
 };
 
-createBooking('ABC123');
-createBooking('ABCD123', 2, 900);
-createBooking('ABCD123', 2);
+createBooking("LH123");
+createBooking('lh123', 2, 800);
+createBooking('LH123', 5)
