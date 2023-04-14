e10d3330c5e457aebdae0fa2fdf349713050bea1
jihun <jxxparker@gmail.com>
2023-04-13 21:26:00 -0500

    pushing 131

--- Fundamental/10/131/script.js
+++ Fundamental/10/131/script.js
@@ -4,32 +4,28 @@ const oneWord = function(str) {
     return str.replace(/ /g, '').toLowerCase();
 };
 
+
 const upperFirstWord = function(str) {
     const [first, ...others] = str.split(' ');
     return [first.toUpperCase(), ...others].join(' ');
+    
 };
 
-//Higher order function
+
+// Higher order function
 const transformer = function(str, fn) {
-    console.log(`Original string ${str}`);
-    console.log(`Transformed string ${fn(str)}`);
-
-    console.log(`Transformed by: ${fn.name}`);
+    console.log(`Original : ${str}`);
+    console.log(`Transformed string: ${fn(str)}`);
+    console.log(`Transformed by ${fn.name}`);
 };
 
-// transformer('Javascript is the best!', upperFirstWord);
-// transformer('Javascript is the best!', oneWord);
 
-// JS uses callbacks all the time
+// transformer('Javascript is the best', upperFirstWord);
+transformer('JAVASCRIPT SUCKS', oneWord);
+
 const high5 = function() {
-    console.log('HIGHFIVE');
-};
-document.body.addEventListener('click', high5);
-['Jihun', 'james', 'Jordan'].forEach(high5);
+    console.log('WAVING');
+}
+document.body.addEventListener('click', high5)
 
-
-
-
-
-
-
+['Jihun', 'James'].forEach(high5);
