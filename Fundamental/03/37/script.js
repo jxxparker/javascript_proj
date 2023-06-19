94fd94ef60b3b1234024ffce644ebda181d15b9e
jihun <jxxparker@gmail.com>
2023-06-18 23:35:19 -0500

    pushing 37

--- Fundamental/03/37/script.js
+++ Fundamental/03/37/script.js
@@ -54,17 +54,36 @@
 
 // console.log(yearsUntilRetirement(1992, 'Jihun'));
 
-function cutFruitPieces (fruit) {
-  return fruit * 4;
+// function cutFruitPieces (fruit) {
+//   return fruit * 4;
 
-}
+// }
 
-function fruitProcessor(apples, oranges) {
-  const applePieces = cutFruitPieces (apples);
-  const orangePieces = cutFruitPieces (oranges);
+// function fruitProcessor(apples, oranges) {
+//   const applePieces = cutFruitPieces (apples);
+//   const orangePieces = cutFruitPieces (oranges);
 
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// };
+
+// console.log(fruitProcessor(2, 3));
+
+const calcAge = function(birthYear){
+  return 2037 - birthYear;
 };
 
-console.log(fruitProcessor(2, 3));
+const yearsUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    return `${firstName} retires in ${retirement} years`;
+  } else {
+    return `${firstName} is retired`;
+  }
+  
+}
+
+console.log(yearsUntilRetirement(1992, 'Jihun'));
+console.log(yearsUntilRetirement(1950, 'James'));
