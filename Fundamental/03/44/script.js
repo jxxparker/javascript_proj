d542fc715e3a1d258024fb8b26b825e26525967b
jihun <jxxparker@gmail.com>
2023-06-22 17:50:31 -0500

    push

---
+++ Fundamental/03/44/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const jonas = {
+  firstName: "Jihun",
+  lastName: "Park",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicense: true,
+
+  calcAge: function(birthYear) {
+    return 2037 - birthYear;
+  }
+};
+
