ce1e31fdf60ce0d72afc66192903a5412be1348a
jihun <jxxparker@gmail.com>
2023-06-21 22:09:13 -0500

    pushing 43

--- Fundamental/03/43/script.js
+++ Fundamental/03/43/script.js
@@ -1,13 +1,5 @@
 "use strict";
 
-const jonasArray = [
-  "Jihun",
-  "Park",
-  2027 - 1991,
-  "Support",
-  ["James", "Jordan", "Peter"],
-];
-
 const jonas = {
   firstName: "Jihun",
   lastName: "Park",
@@ -15,3 +7,30 @@ const jonas = {
   job: "teacher",
   friends: ["Michael", "Peter", "Steven"],
 };
+
+// console.log(jonas);
+// console.log(jonas.lastName);
+// console.log(jonas['lastName']);
+
+// const nameKey = 'Name';
+// console.log(jonas['first' + nameKey]);
+// console.log(jonas['last' + nameKey]);
+
+// const interestedIn = prompt('What do you want to know about Jonas');
+// console.log(interestedIn);
+// console.log(jonas[interestedIn]);
+
+// if (jonas[interestedIn]) {
+//   console.log(jonas[interestedIn]);
+// } else {
+//   console.log('Wrong request');
+// }
+
+jonas.location = 'Korea';
+jonas['instagram'] = '@jihunpark';
+console.log(jonas);
+
+
+//Challenge 
+// Jonas has 3 friends, and his best friend is called Michael
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
