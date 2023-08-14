e44c5bcc466798a6a524a61b9687aa1555013cfa
jihun <jxxparker@gmail.com>
2023-08-13 20:56:31 -0500

    pushinhg

--- Fundamental/07/72/script.js
+++ Fundamental/07/72/script.js
@@ -1,9 +1,13 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'Correct Number';
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
+// document.querySelector('.message').textContent = 'Correct Number';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
 
-document.querySelector('.guess').value = 23;
+// document.querySelector('.gusess').value = 23;
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
--- Fundamental/07/73/script.js
+++ Fundamental/07/73/script.js
@@ -1,9 +1,23 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'Correct Number';
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
+// document.querySelector('.message').textContent = 'Correct Number';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
 
-document.querySelector('.guess').value = 23;
+// document.querySelector('.guess').value = 23;
+
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(typeof guess, guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '❌ No number';
+  }
+});
