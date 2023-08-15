72b90ce4ed58c0cce24427db598816046c421567
jihun <jxxparker@gmail.com>
2023-08-14 20:48:29 -0500

    pushing

--- Fundamental/07/77/script.js
+++ Fundamental/07/77/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 10;
-// document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -18,6 +18,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.background = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // When Guess is too high
   } else if (guess > secretNumber) {
