8412146266469c4413a9ea0f6cc2fadd291f5b16
jihun <jxxparker@gmail.com>
2023-09-06 22:20:22 -0500

    pushing

---
+++ Fundamental/08/95/script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var me = "Jihun";
+let job = "Support";
+const year = 1992;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+// Example
+console.log(undefined);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log("all products deleted");
+}
