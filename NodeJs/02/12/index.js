0aa48d5b10645dcb3986319538708e2d919427aa
jihun <jxxparker@gmail.com>
2023-09-19 21:44:17 -0500

    pushing

---
+++ NodeJs/02/12/index.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+// Server
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the Overview");
+  } else if (pathName === "/product") {
+    res.end("This is the Product");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page is not found</h>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to request on port 8000");
+});
