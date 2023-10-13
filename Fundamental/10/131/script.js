03997f88011e4a80dac8034227b6f06504cc2136
jihun <jxxparker@gmail.com>
2023-10-13 08:32:57 -0500

    pusingZ

--- Fundamental/10/131/script.js
+++ Fundamental/10/131/script.js
@@ -5,6 +5,23 @@ const oneWord = function (str) {
 };
 
 const upperFirstWord = function (str) {
-  const [first, ...others] = str.split("");
+  const [first, ...others] = str.split(" ");
   return [first.toUpperCase(), ...others].join(" ");
 };
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name} `);
+};
+
+transformer("1 ) Javascript is the best!", upperFirstWord);
+transformer("2 ) Javascript is the best!", oneWord);
+
+const high5 = function () {
+  console.log("👍");
+};
+document.body.addEventListener("click", high5);
+
+["Jihun", "Jordan", "James"].forEach(high5);
