887debfc38d40df37b3a386e8c9f756a9fa837bc
jihun <jxxparker@gmail.com>
2023-10-31 21:49:37 -0500

    pushing 209

---
+++ Fundamental/14/209/script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  this.calcAge = function () {
+    console.log(2023 - this.birthYear);
+  };
+};
+
+const jihun = new Person("Jihun", 1992);
+console.log(jihun);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const demar = new Person("Demar", 1993);
+const james = new Person("James", 1935);
+console.log(demar, james);
+
+const jay = "Jay";
+console.log(jihun instanceof Person);
+console.log(jay instanceof Person);
+
+// Prototypes
+Person.prototype;
