5daf1bad49d2a0663eb8956e8e741de6358f551f
jihun <jxxparker@gmail.com>
2023-11-19 18:16:12 -0600

    pushing 23 react

--- ReactJS/04/23/script.js
+++ ReactJS/04/23/script.js
@@ -193,3 +193,28 @@ console.log(`the book has ${pagesRange} pages`);
 
 // const getYear2 = (str) => str.split("-")[0];
 // console.log(getYear2(publicationDate));
+
+// 23 Short circui9ting and logical operatorconsole.log() &&, ||, ??
+
+console.log(true && "Some string");
+console.log(false && "Some string");
+console.log(hasMovieAdaptation && "This book has a movie");
+
+// falsy : 0, '', null, undefined
+console.log("jonas" && "Some string");
+console.log(0 && "Some string");
+
+console.log(true || "Some String");
+console.log(false || "Some String");
+
+console.log(book.translations.spanish);
+
+const spanishTranslation = book.translations.spanish || "Not Translated";
+spanishTranslation;
+
+console.log(book.reviews.librarything.reviewsCount);
+const countWrong = book.reviews.librarything.reviewsCount || "No Data";
+countWrong;
+
+const count = book.reviews.librarything.reviewsCount ?? "No Data";
+count;
