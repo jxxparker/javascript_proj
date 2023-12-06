52d24b9720793294d7aed14952b921a1671ef5f3
jihun <jxxparker@gmail.com>
2023-12-05 20:13:12 -0600

    pushing 46

---
+++ ReactJS/05/44/src/index.js
@@ -0,0 +1,63 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="GlassJames.jpg" alt="Jihun Park" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Jihun Park</h1>
+      <p>
+        I am a Technical Support Engineer at Coates Group. I have a passion for
+        writing beautiful and functional codes. I am adament that the tech world
+        will slow down anytime soon and I should always strive myself to learn
+        and grow each day.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="💪" color="blue" />
+      <Skill skill="HTML+CSS" emoji="💪" color="orange" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
