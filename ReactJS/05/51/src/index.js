2e9a8dc5db77b51f92684036c32d1d428f11457e
jihun <jxxparker@gmail.com>
2024-01-21 20:30:16 -0600

    pushing 52

--- ReactJS/05/51/src/index.js
+++ ReactJS/05/51/src/index.js
@@ -79,6 +79,11 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
+      <p>
+        Authentic Italian Cuisine. 6 creative dishes to choose from. All from
+        our stone oven, all organic, all delicious.
+      </p>
+
       {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
--- ReactJS/05/52/src/index.js
+++ ReactJS/05/52/src/index.js
@@ -80,11 +80,18 @@ function Menu() {
       <h2>Our Menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          <p>
+            Authentic Italian Cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+
+          <ul className="pizzas">
+            {pizzas.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. please come back later</p>
       )}
