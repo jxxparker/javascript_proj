8906b1f4cb19d3ef283ed64bfbb605087028d379
jihun <jxxparker@gmail.com>
2024-03-22 20:53:03 -0500

    pushign 13 nodejs

--- NodeJs/02/13/index.js
+++ NodeJs/02/13/index.js
@@ -1,9 +1,12 @@
 const fs = require("fs");
 const http = require("http");
-const path = require("path");
 const url = require("url");
 
 // server
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   console.log(req.url);
   const pathName = req.url;
@@ -11,6 +14,9 @@ const server = http.createServer((req, res) => {
     res.end("This is the OVERVIEW");
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
