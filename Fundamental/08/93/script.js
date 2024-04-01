929a0c70037c613d0fe7fc54feb3d395d2969174
jihun <jxxparker@gmail.com>
2024-04-01 01:55:10 -0500

    pushing

--- Fundamental/08/93/script.js
+++ Fundamental/08/93/script.js
@@ -4,8 +4,22 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    const output = `You are ${age}, born in ${birthYear}`;
+    let output = `${firstName}, you are ${age} born in ${birthYear}`;
     console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true; // var is function scoped
+      const firstName = "JamesP";
+      const str = `${firstName} is MILLENIAL`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+      output = "newoutput";
+    }
+    console.log(output);
+    // add(2, 3);
   }
   printAge();
   return age;
@@ -13,3 +27,5 @@ function calcAge(birthYear) {
 
 const firstName = "Jihun";
 calcAge(1991);
+// console.log(age);
+// printAge();
