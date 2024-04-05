877287fe5ba543ec253a18ac83ca390f007569b2
jihun <jxxparker@gmail.com>
2024-04-04 21:20:20 -0500

    pushing 98

--- NodeJs/02/10/index.js
+++ NodeJs/02/10/index.js
@@ -1,7 +1,6 @@
 const fs = require("fs");
 
 // Blocking, synchornous way
-
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
 
@@ -10,16 +9,7 @@ fs.writeFileSync("./txt/output.txt", textOut);
 console.log("File written");
 
 // Non Blocking, asynchrous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) return console.log("ERROR");
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
-      console.log(data3);
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-        console.log("your file has been written");
-      });
-    });
-  });
+fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+  console.log(data);
 });
-console.log("will read file");
+console.log("Will read file");
