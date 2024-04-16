655ef49bdb0707da0e9e16762185f525dfb37823
jihun <jxxparker@gmail.com>
2024-04-16 17:19:06 -0500

    pushing

--- Projects/168/script.js
+++ Projects/168/script.js
@@ -1,18 +1,60 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const container = document.querySelector(".container");
-  const daysOfWeek = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    "Sunday",
-  ]; // Array of days
-  for (let i = 0; i < daysOfWeek.length; i++) {
-    const div = document.createElement("div");
-    div.className = "todo-app";
-    div.textContent = daysOfWeek[i]; // Sets the text to the day of the week
-    container.appendChild(div);
+const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+const inputs = days.reduce(
+  (acc, day) => ({
+    ...acc,
+    [day]: document.getElementById(`${day}-input-box`),
+  }),
+  {}
+);
+const lists = days.reduce(
+  (acc, day) => ({
+    ...acc,
+    [day]: document.getElementById(`${day}-list-container`),
+  }),
+  {}
+);
+
+function addTask(day) {
+  const inputBox = inputs[day];
+  const listContainer = lists[day];
+  if (inputBox.value === "") {
+    alert("You must write something");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
   }
+  inputBox.value = "";
+  saveData(day);
+}
+
+Object.values(lists).forEach((listContainer) => {
+  listContainer.addEventListener(
+    "click",
+    function (e) {
+      if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+      } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+      }
+      saveData(listContainer.id.split("-")[0]);
+    },
+    false
+  );
 });
+
+function saveData(day) {
+  localStorage.setItem(`${day}-data`, lists[day].innerHTML);
+}
+
+function showTask() {
+  days.forEach((day) => {
+    lists[day].innerHTML = localStorage.getItem(`${day}-data`);
+  });
+}
+
+showTask();
