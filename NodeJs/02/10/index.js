06c67686ed06554e21c70acd4f52b9214fb38d20
jihun <jxxparker@gmail.com>
2024-06-20 16:43:28 -0500

    pushing 10 node

--- NodeJs/02/10/index.js
+++ NodeJs/02/10/index.js
@@ -1,7 +1,7 @@
 console.log("---");
+const fs = require("fs");
 
 // Blocking, synchronous way
-// const fs = require("fs");
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // // console.log(textIn);
 
@@ -10,4 +10,16 @@ console.log("---");
 // console.log("File written");
 
 // Non-blocking asynchrous way
-fs.readFile();
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}}`, "utf-8", (err) => {
+        console.log("Your file has been written");
+      });
+    });
+  });
+});
+console.log("will read file!");
---
+++ NodeJs/02/11/index.js
@@ -0,0 +1,7 @@
+console.log("---");
+const fs = require("fs");
+const http = require("http");
+
+// Server
+
+http.createServer(() => {});
