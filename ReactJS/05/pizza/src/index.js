be0ea313511fa27dc718f12ac1f0198ffc850bb2
jihun <jxxparker@gmail.com>
2024-08-01 08:20:42 -0500

    pushing reactjs project pizza

--- ReactJS/05/pizza/src/index.js
+++ ReactJS/05/pizza/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,7 +49,7 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -57,15 +58,21 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast React Pizza co.</h1>;
+  // const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+  const style = {};
+  return (
+    <header className="header footer">
+      <h1 style={style}>Fast React Pizza co.</h1>
+    </header>
+  );
 }
 
 function Menu() {
   return (
-    <div>
+    <main className="menu">
       <h2>Our Menu</h2>
       <Pizza />
-    </div>
+    </main>
   );
 }
 
@@ -80,7 +87,9 @@ function Footer() {
   // else alert("Sorry we are closed");
 
   return (
-    <footer>{new Date().toLocaleTimeString()}. We're Currently Open</footer>
+    <footer className="footer">
+      {new Date().toLocaleTimeString()}. We're Currently Open
+    </footer>
   );
   // return React.createElement("footer", null, "We're currently open!");
 }
@@ -89,7 +98,7 @@ function Pizza() {
   return (
     <div>
       <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
