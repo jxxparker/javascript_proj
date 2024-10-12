85f748a752056d49cefed009429d8d3dabf7e61b
jihun <jxxparker@gmail.com>
2024-10-11 22:29:58 -0500

    pushing

--- cryptoplace/src/pages/Home/Home.jsx
+++ cryptoplace/src/pages/Home/Home.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./Home.css";
+import { CoinContext } from "../../context/CoinContext";
 
 const Home = () => {
+  const { allCoin, currency } = useContext(CoinContext);
+  const [displayCoin, setDisplayCoin] = useState([]);
+
+  useEffect(() => {
+    setDisplayCoin(allCoin);
+  }, [allCoin]);
+
   return (
     <div className="home">
       <div className="hero">
         <h1>
           Largest <br /> Crypto Marketplace
         </h1>
-        <p>Welcome to the world's largest cryptocurrency Marketplace</p>
+        <p>Welcome to the world largest cryptocurrency Marketplace</p>
         <form>
           <input type="text" placeholder="Search Crypto .." />
           <button type="submit">Search</button>
@@ -22,6 +30,18 @@ const Home = () => {
           <p style={{ textAlign: "center" }}>24H Change</p>
           <p className="market-cap">Market Cap</p>
         </div>
+        {displayCoin.slice(0, 10).map((item, index) => (
+          <div className="table-layout" key={index}>
+            <p>{item.market_cap_rank}</p>
+            <div>
+              <img src={item.image} alt="" />
+              <p>{item.name + " - " + item.symbol}</p>
+            </div>
+            <p>
+              {currency.symbol} {item.current_price}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
