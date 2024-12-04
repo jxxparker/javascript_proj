25a7955d7eda6450bf76f1a5c81e56371e08b0ce
jihun <jxxparker@gmail.com>
2024-12-03 21:06:01 -0600

    css changes

--- Projects/cryptoplace/src/pages/Home/Home.jsx
+++ Projects/cryptoplace/src/pages/Home/Home.jsx
@@ -40,9 +40,16 @@ const Home = () => {
               <p>{item.name + " - " + item.symbol}</p>
             </div>
             <p>
-              {currency.symbol} {item.current_price}
+              {currency.symbol} {item.current_price.toLocaleString()}
             </p>
-            <p>{item.price_change_percentage_24h}</p>
+            <p
+              className={item.price_change_percentage_24h > 0 ? "green" : "red"}
+            >
+              {Math.floor(item.price_change_percentage_24h * 100) / 100}
+            </p>
+            <p className="market-cap">
+              {currency.symbol} {item.market_cap.toLocaleString()}
+            </p>
           </div>
         ))}
       </div>
