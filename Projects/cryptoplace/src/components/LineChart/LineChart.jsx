fa59dfcc960ad9b73f0b9f90872f0df1771e3974
jihun <jxxparker@gmail.com>
2024-12-11 02:14:00 -0600

    pushing latest

---
+++ Projects/cryptoplace/src/components/LineChart/LineChart.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import Chart from "react-google-charts";
+
+const LineChart = () => {
+  const [data, setData] = useState([["Date", "Prices"]]);
+  return <div></div>;
+};
+
+export default LineChart;
