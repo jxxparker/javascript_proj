ddd6a7e298269afe8215f63fd6d68ad9c1f7b2fc
jihun <jxxparker@gmail.com>
2024-12-11 21:10:42 -0600

    pushing

--- Projects/cryptoplace/src/pages/Coin/Coin.jsx
+++ Projects/cryptoplace/src/pages/Coin/Coin.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import "./Coin.css";
 import { useParams } from "react-router-dom";
 import { CoinContext } from "../../context/CoinContext";
+import LineChart from "../../components/LineChart/LineChart";
 
 const Coin = () => {
   const { coinId } = useParams();
@@ -44,6 +45,7 @@ const Coin = () => {
 
   useEffect(() => {
     fetchCoinData();
+    fetchHistoricalData();
   }, [currency]);
 
   if (coinData && historicalData) {
@@ -56,6 +58,9 @@ const Coin = () => {
               {coinData.name} ({coinData.symbol.toUpperCase()})
             </b>
           </p>
+        </div>
+        <div className="coin-chart">
+          <LineChart historicalData={historicalData} />
         </div>
       </div>
     );
