b78d8a592cef392e8d0f84bc15cccac27bcba0d6
jihun <jxxparker@gmail.com>
2024-12-16 21:20:41 -0600

    pushing 1.9th

--- Projects/ecom_frontend/src/App.js
+++ Projects/ecom_frontend/src/App.js
@@ -20,6 +20,8 @@ function App() {
           <Route path="product" element={<Product />}>
             <Route path=":productId" element={<Product />}></Route>
           </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
         </Routes>
       </BrowserRouter>
     </div>
