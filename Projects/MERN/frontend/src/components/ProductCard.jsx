5f23a378775bceae432d09fc427d1a163b37ee4e
jihun <jxxparker@gmail.com>
2025-01-12 00:27:57 -0600

    pushing new

--- Projects/MERN/frontend/src/components/ProductCard.jsx
+++ Projects/MERN/frontend/src/components/ProductCard.jsx
@@ -37,6 +37,24 @@ const ProductCard = ({ product }) => {
         w="full"
         objectFit="cover"
       />
+      <Box p={4}>
+        <Heading as="h3" size="md" mb={2}>
+          {product.name}
+        </Heading>
+
+        <Text fontWeight="bold" fontSize="xl" color={textColor} mb={4}>
+          ${product.price}
+        </Text>
+
+        <HStack spacing={2}>
+          <IconButton icon={<EditIcon />} onClick={onOpen} colorScheme="blue" />
+          <IconButton
+            icon={<DeleteIcon />}
+            onClick={() => handleDeleteProduct(product._id)}
+            colorScheme="red"
+          />
+        </HStack>
+      </Box>
     </Box>
   );
 };
