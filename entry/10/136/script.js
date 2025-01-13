3a8ab197b19f93eafffc7803a2f7228f25c0a28c
jihun <jxxparker@gmail.com>
2025-01-12 21:12:57 -0600

    pushing 136

--- entry/10/136/script.js
+++ entry/10/136/script.js
@@ -3,3 +3,26 @@
 const oneWord = function (str) {
   return str.replace(/ /g, "").toLowerCase();
 };
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+// higher order function
+const transformer = function (str, fn) {
+  console.log(`1: original string: ${str}`);
+  console.log(`2: transformed string: ${fn(str)}`);
+  console.log(`3: transformed by: ${fn.name}`);
+};
+
+// transformer("Javascript is the best", upperFirstWord);
+// transformer("Javascript is the best", oneWord);
+
+const high5 = function () {
+  console.log("HIGH");
+};
+
+document.body.addEventListener("click", high5);
+
+["Jihun", "Jordan", "Kobe"].forEach(high5);
