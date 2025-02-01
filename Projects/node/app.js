6b8cd32b4dbd20ab536847e3ec23981fd5552dcb
jihun <jxxparker@gmail.com>
2025-01-31 22:48:33 -0600

    pushing

--- Projects/node/app.js
+++ Projects/node/app.js
@@ -1,1 +1,18 @@
-console.log("hello");
+const fs = require("fs");
+
+// fs.readFile("example.txt", "utf8", (err, data) => {
+//   if (err) {
+//     console.error(error);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+const content = "Hello, NodeJS";
+fs.writeFile("output.txt", content, (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log("File written successfully");
+});
