46f44f9bdfe172cb96b34a4251b6c2bf8eae499d
jihun <jxxparker@gmail.com>
2025-02-07 15:37:50 -0600

    pushing

--- Projects/node/course/4_OS/app.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const http = require("http");
-const path = require("path");
-
-const readableStream = fs.createWriteStream("");
-
-const writeableStream = fs.createWriteStream("example-output.txt");
-
-readableStream.pipe(writeableStream);
