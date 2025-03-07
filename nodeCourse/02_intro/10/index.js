e684efbe9d1fe07f134fc29d1d8c8c274f22b7b4
jihun <jxxparker@gmail.com>
2025-03-07 15:47:33 -0600

    pushing

--- nodeCourse/02_intro/10/index.js
+++ nodeCourse/02_intro/10/index.js
@@ -1,14 +1,24 @@
 const fs = require("fs");
 
-// Blocking synchrous
-const textIn = fs.readFile("./txt/input.txt", "utf-8");
-console.log(textIn);
+// // Blocking synchrous
+// const textIn = fs.readFile("./txt/input.txt", "utf-8");
+// console.log(textIn);
 
-const textOut = `This is what we know about avocado. ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File written");
+// const textOut = `This is what we know about avocado. ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written");
 
 // Non-blocking, asynchrouns way
-fs.readFile("./txt/start.txt", (err, data) => {
-  console.log(data);
+fs.readFile("./txt/start.txt", (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("Your file has been written!!!@@@@");
+      });
+    });
+  });
 });
+console.log("Will read file");
