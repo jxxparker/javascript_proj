dc5f125699733903a7529ca2e22f8bf6288a9f36
jihun <jxxparker@gmail.com>
2025-03-15 13:48:32 -0500

    pushing new

--- nodeCourse/02_intro/11/index.js
+++ nodeCourse/02_intro/11/index.js
@@ -3,7 +3,10 @@ const http = require("http");
 
 // Server
 const server = http.createServer((req, res) => {
+  console.log(req);
   res.end("hello from the server");
 });
 
-server.listen(8000, "127.0.0.1");
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to requests on port 8000");
+});
