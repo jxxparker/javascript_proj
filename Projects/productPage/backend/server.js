412204f71430bd59a4ac5a1324b1e19ead45a679
jihun <jxxparker@gmail.com>
2025-04-02 11:34:04 -0500

    pushing

--- Projects/productPage/backend/server.js
+++ Projects/productPage/backend/server.js
@@ -1,13 +1,27 @@
-//password tjMkTGKkTvkxpnox
-
 import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { connectDB } from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.post("/products", (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+});
 
 console.log(process.env.MONGO_URI);
 
 app.listen(5000, () => {
+  connectDB();
   console.log("server is running on localhost:5000");
 });
+
+//password tjMkTGKkTvkxpnox
