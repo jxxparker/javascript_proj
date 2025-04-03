a565fdd6f8d563df5a8ac21ebf265f9260afcad3
jihun <jxxparker@gmail.com>
2025-04-02 20:16:11 -0500

    pushing new backend for productpage

--- Projects/productPage/backend/server.js
+++ Projects/productPage/backend/server.js
@@ -2,26 +2,30 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.post("/products", (req, res) => {
-  const product = req.body;
+const __dirname = path.resolve();
 
-  if (!product.name || !product.price || !product.image) {
-    return res
-      .status(400)
-      .json({ success: false, message: "Please provide all fields" });
-  }
-});
+app.use(express.json()); // it allows us to accept JSON data in the body
 
-console.log(process.env.MONGO_URI);
+app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+// console.log(process.env.MONGO_URI);
+app.listen(PORT, () => {
   connectDB();
-  console.log("server is running on localhost:5000");
+  console.log("Server started at http://localhost:" + PORT);
 });
 
 //password tjMkTGKkTvkxpnox
